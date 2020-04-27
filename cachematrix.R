9b9ebdd45ea064642634a74882c5b28a45c9cae0
Neha1685 <neha.chaudhary1685@gmail.com>
2020-04-26 23:27:26 -0400

    Added a Note for input to be an invertible matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,13 +1,22 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 # Below are two functions that are used to create a special object that stores a square invertible matrix and cache's its inverse.
+##The cachematrix.R file contains two functions, makeCacheMatrix() and cachemean(). 
+##The first function in the file, makeCacheMatrix() creates an R object that stores a matrix and its inverse. 
+##The second function, cacheSolve() requires an argument that is returned by makeCacheMatrix() in order to 
+##retrieve the inverse from the cached value that is stored in the makeCacheMatrix() object's environment.
+
 
 ## Write a short comment describing this function
+
 #The first function, makeCacheMatrix creates a special "vector",
 #which is really a list containing a function to set the value of 
 #the matrix,get the value of the matrix, set the value of the 
 #inverse, get the value of the inverse
+
+## NOTE: please enter an invertible matrix
 makeCacheMatrix <- function(x = matrix()) {
+    
     i <- NULL
     set <- function(y = matrix) {
         x <<- y
@@ -27,6 +36,7 @@ makeCacheMatrix <- function(x = matrix()) {
 #calculated. If so, it gets the inverse from the cache and skips 
 #the computation. Otherwise, it calculates the inverse of the data 
 #and sets the value of the inverse in the cache via the setinverse function.
+
 cacheSolve <- function(x, ...) {
     i <- x$getinverse()
     if(!is.null(i)) {
